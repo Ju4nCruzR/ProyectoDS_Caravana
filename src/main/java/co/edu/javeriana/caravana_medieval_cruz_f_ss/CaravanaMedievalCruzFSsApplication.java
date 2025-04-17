287bc9df1aff61f1aae5b35822e82ce7e6139c83
Juan S. Cruz <juan.cruzr1709@gmail.com>
2025-04-17 17:19:10 -0500

    Primera versión del proyecto Caravana Medieval

---
+++ src/main/java/co/edu/javeriana/caravana_medieval_cruz_f_ss/CaravanaMedievalCruzFSsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.caravana_medieval_cruz_f_ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaravanaMedievalCruzFSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaravanaMedievalCruzFSsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/caravana_medieval_cruz_f_ss/CaravanaMedievalCruzFSsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.caravana_medieval_cruz_f_ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanaMedievalCruzFSsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
