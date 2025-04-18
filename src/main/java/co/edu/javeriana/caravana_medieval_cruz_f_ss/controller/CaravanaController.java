e34115ed0820ee859fba6c67dba94ec6c160ca5a
Juan S. Cruz <juan.cruzr1709@gmail.com>
2025-04-18 15:24:55 -0500

    Caravana Service y controller con el primer caso de uso de una caravana (listarCaravanas) funcionando y sin errores

--- src/main/java/co/edu/javeriana/caravana_medieval_cruz_f_ss/controller/CaravanaController.java
+++ src/main/java/co/edu/javeriana/caravana_medieval_cruz_f_ss/controller/CaravanaController.java
@@ -1,10 +1,15 @@
 package co.edu.javeriana.caravana_medieval_cruz_f_ss.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.edu.javeriana.caravana_medieval_cruz_f_ss.model.Caravana;
 import co.edu.javeriana.caravana_medieval_cruz_f_ss.service.CaravanaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/caravana")
@@ -12,5 +17,12 @@ public class CaravanaController {
     @Autowired
     private CaravanaService caravanaService;
 
+    @GetMapping("/list")
+    public ModelAndView listarCaravanas() {
+        List<Caravana> caravanas = caravanaService.listarCaravanas();
+        ModelAndView modelAndView = new ModelAndView("caravana-list");
+        modelAndView.addObject("listaCaravanas", caravanas);
+        return modelAndView;
+    } 
 
 }
--- src/main/java/co/edu/javeriana/caravana_medieval_cruz_f_ss/service/CaravanaService.java
+++ src/main/java/co/edu/javeriana/caravana_medieval_cruz_f_ss/service/CaravanaService.java
@@ -1,8 +1,11 @@
 package co.edu.javeriana.caravana_medieval_cruz_f_ss.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.javeriana.caravana_medieval_cruz_f_ss.model.Caravana;
 import co.edu.javeriana.caravana_medieval_cruz_f_ss.repository.CaravanaRepository;
 
 @Service
@@ -10,5 +13,7 @@ public class CaravanaService {
     @Autowired
     private CaravanaRepository caravanaRepository;
 
-
+    public List<Caravana> listarCaravanas() {
+        return caravanaRepository.findAll();
+    }
 }
